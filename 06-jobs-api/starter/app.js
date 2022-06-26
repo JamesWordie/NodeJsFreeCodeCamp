8455e29581352dffd27b54e7ba742359d69a9ca1
James Wordie <james.wordie@yahoo.com>
2022-06-26 09:05:14 +0100

    completed the project but didn't publish

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -3,6 +3,12 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+// Security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 // Connect DB
 const connectDB = require("./db/connect");
 
@@ -16,8 +22,18 @@ const authenticateUser = require("./middleware/authentication");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1); // setup for heroku
 app.use(express.json());
 // extra packages
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 Minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
